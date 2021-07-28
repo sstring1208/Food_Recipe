1ed1c753c4cbe29e4ecebc9d7dfd31f67085bb28
Shubham Singh <shubhamsingh@Shubhams-MacBook-Pro.local>
2021-07-29 00:43:32 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import Axios from "axios";
+import { useState } from "react";
+import "./app.css";
+import RecipeTile from "./RecipeTile";
+import {YOUR_APP_ID,YOUR_APP_KEY } from "./key";
+
+
 
 function App() {
+  const [query, setquery] = useState("");
+  const [recipes, setrecipes] = useState([]);
+
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+
+  const getRecipeInfo = async () => {
+    var result = await Axios.get(url);
+    setrecipes(result.data.hits);
+    console.log(result.data.hits);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getRecipeInfo();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 onClick={getRecipeInfo}>Food Recipe🍔</h1>
+      <form className="app__searchForm" onSubmit={onSubmit}>
+        <input
+          className="app__input"
+          type="text"
+          placeholder="search ingridient"
+          autoComplete="Off"
+          value={query}
+          onChange={(e) => setquery(e.target.value)}
+        />
+        <input className="app__submit" type="submit" value="Search" />
+      </form>
+
+      <div className="app__recipes">
+        {recipes !== [] &&
+          recipes.map((recipe) => {
+            return <RecipeTile recipe={recipe} />;
+          })}
+      </div>
+     
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './footer.css';
+
+function Footer() {
+    return (
+        <div className="footer">
+
+          <p>Created By Shubham Singh 2021</p> 
+            
+        </div>
+    )
+}
+
+export default Footer
